d220c44c43643d3161a0586bead4ab5faffa92ac
Clément Vazeille <118741151+Clement-Vazeille@users.noreply.github.com>
2023-08-18 22:26:46 +0200

    Time: 119 ms (16.78%), Space: 111.3 MB (13.75%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-
-    void backtrack(vector<vector<int>>& res,int n,int k,int current,vector<int>& vec){
-        if(0==k){
-            res.push_back(vec);
-            return;
-        }
-
-        if(n-current>=k){backtrack(res,n,k,current+1,vec);}
-        
-        vec.push_back(current);
-        backtrack(res,n,k-1,current+1,vec);
-
-        vec.pop_back();
-    }
-
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> res;
-        vector<int> vec;
-        backtrack(res,n,k,1,vec);
+        vector<vector<int>>res;
+        vector<int> comb;
+        backtrack(res,1,n,k,comb);
         return res;
     }
-
     
+    void backtrack(vector<vector<int>>& res, int cur, int n, int k, vector<int>& comb){
+        if(k==0){
+            res.push_back(comb);
+            return;
+        }
+        if(cur<=n-k) backtrack(res,cur+1,n,k,comb);  
+        comb.push_back(cur);
+        backtrack(res,cur+1,n,k-1,comb);
+        comb.pop_back();
+    }
 };
