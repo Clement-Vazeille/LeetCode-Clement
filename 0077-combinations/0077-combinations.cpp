a3a69b0b21cdee5bc256856dad12771ba9c230ce
Clément Vazeille <118741151+Clement-Vazeille@users.noreply.github.com>
2023-08-19 22:25:07 +0200

    Time: 102 ms (19.17%), Space: 59.6 MB (17.3%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> combination(k);
+        generateCombinations(1, n, k, combination, result);
+        return result;
+    }
+
+private:
+    void generateCombinations(int start, int n, int k, vector<int> &combination, vector<vector<int>> &result) {
+        if (k == 0) {
+            result.push_back(combination);
+            return;
+        }
+        for (int i = start; i <= n; ++i) {
+            combination[combination.size() - k] = i;
+            generateCombinations(i + 1, n, k - 1, combination, result);
+        }
+    }
+};
+
+/*class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> res;
         vector<int> vec;
         backtrack(res,n,k,1,vec);
@@ -23,4 +45,4 @@ private:
     }
 
     
-};
+}; */
