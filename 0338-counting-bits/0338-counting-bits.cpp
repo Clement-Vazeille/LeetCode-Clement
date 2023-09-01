f031d29cdd92cfef10c6cbb7f739de2637450dd9
Clément Vazeille <118741151+Clement-Vazeille@users.noreply.github.com>
2023-09-01 18:27:41 +0200

    Time: 3 ms (89.82%), Space: 7.8 MB (89.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,10 +3,21 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1);
         ans[0]=0;
+        int power=2;
+        int taille=1;
         for(int i=1;i<=n;i++)
         {
             if(i%2==1)ans[i]=ans[i-1]+1;
-            else ans[i]=__builtin_popcount(i);
+            else if(i==power)
+            {
+                power*=2;
+                ans[i]=1;
+                taille++;
+            }
+            else 
+            {
+                ans[i]=2*ans[i&(i-1)]+ans[i^(i-1)]-ans[i-1];
+            }
         }
         return ans;
     }
